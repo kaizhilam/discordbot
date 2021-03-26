0dda9e34be261ea82b0996a31c493e88a55a4020
kaizhilam <kaizhilam@gmail.com>
2021-03-26 20:52:43 +1100

    Added unit test and debugger for it

--- src/events/message/__tests__/message.spec.ts
+++ src/events/message/__tests__/message.spec.ts
@@ -1,4 +1,7 @@
 import { message } from '../message';
+import { actionManager } from '../../../actions';
+
+jest.mock('../../../actions');
 
 describe('message', () => {
   const mockOn = jest.fn();
@@ -11,11 +14,89 @@ describe('message', () => {
     jest.restoreAllMocks();
   });
 
-  it('SHOULD be called with message for bot', () => {
+  it("SHOULD be called with 'message' for bot.on", () => {
     const payloads = [];
     // @ts-ignore
     message({ bot, payloads });
     const mockOnFirstArg = mockOn.mock.calls[0][0];
     expect(mockOnFirstArg).toEqual('message');
+  });
+
+  it('SHOULD run action manager when message content matches with ignorecase', () => {
+    const msg = {
+      content: 'TeSt',
+    };
+    const payloads = [
+      {
+        event: 'message',
+        args: {
+          message: 'tEsT',
+          ignoreCase: true,
+        },
+      },
+    ];
+    // @ts-ignore
+    message({ bot, payloads });
+    const mockOnSecondArg = mockOn.mock.calls[0][1];
+    mockOnSecondArg(msg);
+    expect(actionManager).toHaveBeenCalledTimes(1);
+  });
+
+  it('SHOULD NOT run action manager when message content does not match even with ignorecase', () => {
+    const msg = {
+      content: 'TeSt',
+    };
+    const payloads = [
+      {
+        event: 'message',
+        args: {
+          message: 'AsDf',
+          ignoreCase: true,
+        },
+      },
+    ];
+    // @ts-ignore
+    message({ bot, payloads });
+    const mockOnSecondArg = mockOn.mock.calls[0][1];
+    mockOnSecondArg(msg);
+    expect(actionManager).toHaveBeenCalledTimes(0);
+  });
+
+  it('SHOULD run action manager when message content matches', () => {
+    const msg = {
+      content: 'test',
+    };
+    const payloads = [
+      {
+        event: 'message',
+        args: {
+          message: 'test',
+        },
+      },
+    ];
+    // @ts-ignore
+    message({ bot, payloads });
+    const mockOnSecondArg = mockOn.mock.calls[0][1];
+    mockOnSecondArg(msg);
+    expect(actionManager).toHaveBeenCalledTimes(1);
+  });
+
+  it('SHOULD NOT run action manager when message content does not match', () => {
+    const msg = {
+      content: 'test',
+    };
+    const payloads = [
+      {
+        event: 'message',
+        args: {
+          message: 'asdf',
+        },
+      },
+    ];
+    // @ts-ignore
+    message({ bot, payloads });
+    const mockOnSecondArg = mockOn.mock.calls[0][1];
+    mockOnSecondArg(msg);
+    expect(actionManager).toHaveBeenCalledTimes(0);
   });
 });
