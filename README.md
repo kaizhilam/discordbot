0891b79023a0c30e4b32e583c967d5271268f4cc
kaizhilam <kaizhilam@gmail.com>
2021-03-27 19:19:06 +1100

    added server

--- README.md
+++ README.md
@@ -13,10 +13,49 @@ Just a simple Discord bot
 ```
 TOKEN=[TOKEN HERE]
 GUILD_ID=[GUILD ID HERE]
+SCHEMA_HOST=[ENDPOINT]
+PORT=[PORT]
 ```
 2. Install all dependencies `npm install`
 3. Build packages `npm run build`
 4. Start the bot `npm run start`
+
+### Useage
+
+1. Send a POST request to SCHEMA_HOST:PORT with schema for body
+
+### Example schema
+
+```
+[
+    {
+        "name": "Hello World",
+        "event": "message",
+        "args": {
+            "message": "hello",
+        },
+        "actions": [
+            {
+                "name": "reply world",
+                "action": "sendToChannel",
+                "args": {
+                    "message": "World"
+                }
+            }
+        ]
+    }
+]
+```
+
+### Example .env
+
+```
+TOKEN=AAAABBBBCCCCDDDD1111222233334444
+GUILD_ID=1234567890
+SCHEMA_HOST=http://localhost
+PORT=3000
+```
+
 
 ### Running the tests
 
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,35 @@
 import { config as dotEnvConfig } from 'dotenv';
 import { Client } from 'discord.js';
-import fs from 'fs';
 import { voiceStateUpdate, message, ready } from './events';
 import { IConfig } from './utils';
+import * as express from 'express';
 
 dotEnvConfig();
 
-const bot = new Client();
 const TOKEN = process.env.TOKEN;
 const GUILD_ID = process.env.GUILD_ID;
+const SCHEMA_HOST = process.env.SCHEMA_HOST;
+const PORT = process.env.PORT;
 const config = {
   token: TOKEN,
   guildId: GUILD_ID,
 } as IConfig;
 
+const bot = new Client();
 bot.login(TOKEN);
 
-const rawdata = fs.readFileSync('payload.json', 'utf8');
-const externalPayload = JSON.parse(rawdata);
-
-bot.guilds.fetch(GUILD_ID).then((guild) => {
-  ready({ bot, payloads: externalPayload, config, guild });
-  message({ bot, payloads: externalPayload, config, guild });
-  voiceStateUpdate({ bot, payloads: externalPayload, config, guild });
+const app = express();
+app.use(express.json());
+app.post('/', (req, res) => {
+  const payloads = req.body;
+  bot.removeAllListeners();
+  bot.guilds.fetch(GUILD_ID).then((guild) => {
+    ready({ bot, payloads, config, guild });
+    message({ bot, payloads, config, guild });
+    voiceStateUpdate({ bot, payloads, config, guild });
+  });
+  res.sendStatus(200);
+});
+app.listen(PORT, () => {
+  console.log(`Listening at ${SCHEMA_HOST}:${PORT}`);
 });
