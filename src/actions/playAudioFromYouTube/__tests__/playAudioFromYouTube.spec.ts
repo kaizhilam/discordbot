8b63ee0243e5374345d4aded44587deaf049e686
kaizhilam <kaizhilam@gmail.com>
2021-04-02 13:41:43 +1100

    Play audio from youtube action (#19)

--- src/actions/playAudioFromYouTube/__tests__/playAudioFromYouTube.spec.ts
+++ src/actions/playAudioFromYouTube/__tests__/playAudioFromYouTube.spec.ts
@@ -1,10 +1,15 @@
 import { playAudioFromYouTube } from '..';
 import ytdl from 'discord-ytdl-core';
+import 'regenerator-runtime/runtime';
 
 jest.mock('discord-ytdl-core');
 
 describe('playAudioFromYouTube', () => {
-  it('SHOULD play music', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    jest.restoreAllMocks();
+  });
+  it('SHOULD play audio', () => {
     const mockOn = jest.fn();
     const mockPlay = jest.fn();
     const mockJoin = jest.fn();
@@ -33,8 +38,116 @@ describe('playAudioFromYouTube', () => {
         url: 'url',
       },
     };
-    // @ts-ignore
     playAudioFromYouTube(props);
     expect(ytdl.mock.calls[0][0]).toEqual(props.args.url);
+  });
+
+  it('SHOULD play on start time when start time is given', () => {
+    const mockOn = jest.fn();
+    const mockPlay = jest.fn();
+    const mockJoin = jest.fn();
+    mockOn.mockImplementation((arg1, arg2) => arg2());
+    mockJoin.mockResolvedValue({
+      play: mockPlay,
+    });
+    mockPlay.mockReturnValue({
+      on: mockOn,
+    });
+    const mockLeave = jest.fn();
+    const props = {
+      guild: {
+        channels: {
+          cache: [
+            {
+              name: 'channel',
+              join: mockJoin,
+              leave: mockLeave,
+            },
+          ],
+        },
+      },
+      args: {
+        channel: 'channel',
+        start: 10,
+        url: 'url',
+      },
+    };
+    // @ts-ignore
+    playAudioFromYouTube(props);
+    expect(ytdl.mock.calls[0][1]['seek']).toEqual(props.args.start);
+  });
+
+  it('SHOULD stop audio when a stop time is given', async () => {
+    const mockOn = jest.fn();
+    const mockPlay = jest.fn();
+    const mockJoin = jest.fn();
+    mockOn.mockImplementation((arg1, arg2) => arg2());
+    mockJoin.mockResolvedValue({
+      play: mockPlay,
+    });
+    mockPlay.mockReturnValue({
+      on: mockOn,
+    });
+    const mockLeave = jest.fn();
+    const props = {
+      guild: {
+        channels: {
+          cache: [
+            {
+              name: 'channel',
+              join: mockJoin,
+              leave: mockLeave,
+            },
+          ],
+        },
+      },
+      args: {
+        channel: 'channel',
+        url: 'url',
+        stop: 5,
+      },
+    };
+    jest.useFakeTimers();
+    await playAudioFromYouTube(props);
+    expect(setTimeout.mock.calls[0][1]).toEqual(5000);
+    expect(setTimeout).toHaveBeenCalledTimes(1);
+    mockLeave.mockReset();
+    setTimeout.mock.calls[0][0]();
+    expect(mockLeave).toHaveBeenCalledTimes(1);
+  });
+
+  it('SHOULD do nothing if stop time is greater than start time', () => {
+    const mockOn = jest.fn();
+    const mockPlay = jest.fn();
+    const mockJoin = jest.fn();
+    mockOn.mockImplementation((arg1, arg2) => arg2());
+    mockJoin.mockResolvedValue({
+      play: mockPlay,
+    });
+    mockPlay.mockReturnValue({
+      on: mockOn,
+    });
+    const mockLeave = jest.fn();
+    const props = {
+      guild: {
+        channels: {
+          cache: [
+            {
+              name: 'channel',
+              join: mockJoin,
+              leave: mockLeave,
+            },
+          ],
+        },
+      },
+      args: {
+        channel: 'channel',
+        start: 10,
+        url: 'url',
+        stop: 5,
+      },
+    };
+    playAudioFromYouTube(props);
+    expect(ytdl).toHaveBeenCalledTimes(0);
   });
 });
--- src/actions/playAudioFromYouTube/playAudioFromYouTube.ts
+++ src/actions/playAudioFromYouTube/playAudioFromYouTube.ts
@@ -5,16 +5,28 @@ import ytdl from 'discord-ytdl-core';
 export function playAudioFromYouTube(props: IActionProps): void {
   const { guild, args } = props;
   const { channel, url } = args;
+  let { start, stop } = args;
+  start = start || 0;
+  stop = stop || Infinity;
+  if (start > stop) return;
   const chosenChannel = guild.channels.cache.find((c) => c.name === channel) as VoiceChannel;
   const stream = ytdl(url, {
     filter: 'audioonly',
-    opusEncoded: true,
+    opusEncoded: false,
+    fmt: 'mp3',
     encoderArgs: ['-af', 'bass=g=10,dynaudnorm=f=200'],
+    seek: start ? start : undefined,
   });
   chosenChannel.join().then((connection) => {
+    if (stop !== Infinity) {
+      stop = stop - start;
+      setTimeout(() => {
+        chosenChannel.leave();
+      }, stop * 1000);
+    }
     connection
       .play(stream, {
-        type: 'opus',
+        type: 'unknown',
       })
       .on('finish', () => {
         chosenChannel.leave();
